d6fb467e9db4ae355975560f675a8d7a2c90ba53
chetan-redkivi <cheyan.tomar@redkivi.com>
2013-01-17 14:22:02 +0530

    modifications

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,35 +3,40 @@ class HomeController < ApplicationController
     if user_signed_in?
       token,uid = initialise_objects()
       @graph = Koala::Facebook::API.new("#{token}")
-      @friends = @graph.get_connections("#{uid}","friends")
-      @groups = @graph.get_connections("#{uid}", "groups")
-      @pages = @graph.get_connections("#{uid}", "likes")
+      @friends = @graph.get_connections("#{uid}","friends","fields" => "id,name,picture,link")
+      @groups = @graph.get_connections("#{uid}", "groups","fields" => "id,name,picture,link")
+      @pages = @graph.get_connections("#{uid}", "likes","fields" => "id,name,picture,link")
+
     end
   end
 
   def post_on_page
     all_ids = []
     begin
-      if params["pageId"].present?
-        all_ids = all_ids + params["pageId"]
-      end
-
-      if params["groupId"].present?
-        all_ids = all_ids + params["groupId"]
-      end
-
-      if params["friendId"].present?
-        all_ids = all_ids + params["friendId"]
-      end
-
-      if all_ids.present?
-        token,uid = initialise_objects()
-        @graph = Koala::Facebook::API.new("#{token}")
-        all_ids.each do |id|
-          @graph.put_connections("#{id}", "feed", :message => params["message"])
+      if params["message"].present?
+        if params["pageId"].present?
+          all_ids = all_ids + params["pageId"]
         end
+
+        if params["groupId"].present?
+          all_ids = all_ids + params["groupId"]
+        end
+
+        if params["friendId"].present?
+          all_ids = all_ids + params["friendId"]
+        end
+
+        if all_ids.present?
+          token,uid = initialise_objects()
+          @graph = Koala::Facebook::API.new("#{token}")
+          all_ids.each do |id|
+            @graph.put_connections("#{id}", "feed", :message => params["message"])
+          end
+        end
+        flash[:notice] = "Message has been posted successfully."
+      else
+        flash[:error] = "Message should not be blank"
       end
-      flash[:notice] = "Message has been posted successfully."
     rescue Exception => e
       Rails.logger.info("=========================#{e.message}")
       flash[:error] = e.message

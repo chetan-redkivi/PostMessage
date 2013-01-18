72c08c55af5c1fdcb749560acc4316f34071591a
chetan-redkivi <cheyan.tomar@redkivi.com>
2013-01-18 14:23:37 +0530

    sorting done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,74 @@ class HomeController < ApplicationController
     if user_signed_in?
       token,uid = initialise_objects()
       @graph = Koala::Facebook::API.new("#{token}")
-      @friends = @graph.get_connections("#{uid}","friends","fields" => "id,name,picture,link")
-      @groups = @graph.get_connections("#{uid}", "groups","fields" => "id,name,picture,link")
-      @pages = @graph.get_connections("#{uid}", "likes","fields" => "id,name,picture,link")
+      friends_profile = @graph.get_connections("#{uid}","friends","fields" => "id,name,picture,link")
+      fetch_sorted_friends_profile(friends_profile)
 
+      fetch_groups = @graph.get_connections("#{uid}", "groups","fields" => "id,name,picture,link")
+      fetch_sorted_groups(fetch_groups)
+
+      fetch_pages = @graph.get_connections("#{uid}", "likes","fields" => "id,name,picture,link")
+      fetch_sorted_pages(fetch_pages)
+    end
+  end
+
+ def fetch_sorted_pages(fetch_pages)
+   @pages = []
+   if fetch_pages.present?
+     fetch_pages.each do |page|
+       pg = {}
+       pg["id"] = page["id"]
+       pg["name"] = page["name"]
+       pg["link"] = page["link"]
+       pg["picture"] = page["picture"]["data"]["url"]
+       @pages << pg
+     end
+     unless @pages.blank?
+       @pages = @pages.sort_by { |hsh| hsh["name"] }
+     end
+   end
+ end
+
+
+ def fetch_sorted_groups(fetch_groups)
+    @groups = []
+    if fetch_groups.present?
+      fetch_groups.each do |group|
+        grp = {}
+        grp["id"] = group["id"]
+        grp["name"] = group["name"]
+        if group["picture"]["is_silhouette"]
+          @picture = group["picture"]["data"]["url"]
+        else
+          @picture = "default-group.png"
+        end
+        grp["link"] = group["link"]
+        grp["picture"] = @picture
+        @groups << grp
+      end
+      unless @groups.blank?
+        @groups = @groups.sort_by { |hsh| hsh["name"] }
+      end
+
+    end
+  end
+
+  def fetch_sorted_friends_profile(friends_profile)
+    @friends_profile = []
+    if friends_profile.present?
+      friends_profile.each do |friend|
+        friend_profile = {}
+        friend_profile["id"] = friend["id"]
+        friend_profile["name"] = friend["name"]
+        friend_profile["link"] = friend["link"]
+        friend_profile["picture"] = friend["picture"]["data"]["url"]
+        unless friend_profile.blank?
+          @friends_profile << friend_profile
+        end
+      end
+      unless @friends_profile.blank?
+        @friends_profile = @friends_profile.sort_by { |hsh| hsh["name"] }
+      end
     end
   end
 
